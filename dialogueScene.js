bdfe25829b7ebdee51ea3e476ce9aa7e17f0186c
Adrian <adrian@Adrians-MacBook-Pro.local>
2021-05-07 13:33:24 -0400

    add quiz

--- dialogueScene.js
+++ dialogueScene.js
@@ -6,30 +6,11 @@ class dialogueScene extends Phaser.Scene{
 
     init(){
         //answers database
-        
-    }
-
-
-    preload(){
-        
-      
-    }
-    create(){       
-
-        this.question = this.add.text(180, 480, 'Imaging you are a friend of Allan, What you are willing to do?',
-        {backgroundColor: "#EFCA8F",
-        padding: 15,
-        color: "black"});
-
-        this.intro = this.add.image(1050, game.config.height/4,'intro').setScale(0.7),
-        
-        
-
-    this.choices = this.add.group([{
+        this.answers = [{
             key:'choice1',
             setXY:{
                 x: 250,
-                y: game.config.height/3*2,
+                y: game.config.height/3*2-50,
                 },
             setScale:{
                 x: 0.5,
@@ -40,7 +21,7 @@ class dialogueScene extends Phaser.Scene{
             key:'choice2',
             setXY:{
                 x: 450,
-                y: game.config.height/3*2,
+                y: game.config.height/3*2-50,
                 },
             setScale:{
                 x: 0.5,
@@ -51,7 +32,7 @@ class dialogueScene extends Phaser.Scene{
             key:'choice3',
             setXY:{
                 x: 650,
-                y: game.config.height/3*2,
+                y: game.config.height/3*2-50,
                 },
             setScale:{
                 x: 0.5,
@@ -62,15 +43,33 @@ class dialogueScene extends Phaser.Scene{
             key:'choice4',
             setXY:{
                 x: 850,
-                y: game.config.height/3*2,
+                y: game.config.height/3*2-50,
                 },
             setScale:{
                 x: 0.5,
                 y: 0.5,
                 },
-            },
-            
-    ],);
+            },]
+        
+    }
+
+
+    preload(){
+        
+      
+    }
+    create(){       
+
+        this.question = this.add.text(180, 420, 'Imaging you are a friend of Allan, What you are willing to do?',
+        {backgroundColor: "#EFCA8F",
+        padding: 15,
+        color: "black"});
+
+        this.intro = this.add.image(1050, game.config.height/4,'intro').setScale(0.7),
+        
+        
+
+    this.choices = this.add.group(this.answers);
     
     Phaser.Actions.Call(this.choices.getChildren(), function(choices) {
             //make the choices interactive
@@ -131,13 +130,41 @@ class dialogueScene extends Phaser.Scene{
         
         if(userResponse == this.answers[0]){
             
-            this.result1 = this.add.text(500, game.config.height-200, 'Your message made Allan feel better. He replied ‘Thanks’ to you. But in the last few days, you find that Alan is still unhappy. He always stays alone in his seat. You are wondering if there is anything else you can do...')
+            this.result1 = this.add.text(500, game.config.height-200, 'Your message made Allan feel better. He replied ‘Thanks’ to you. But in the last few days, you find that Alan is still unhappy. He always stays alone in his seat. You are wondering if there is anything else you can do... Try it again to help Allan!',{
+            backgroundColor: "#EFCA8F",
+            padding: 15,
+            color: "black",
+            alpha: 1,
+            wordWrap: { width: 450, useAdvancedWrap: true }
+            })
+            setTimeout(function(){ result1.alpha = 0}, 500)
         }else if(userResponse == this.answers[1]){
-            this.result2 = this.add.text(500, game.config.height-200, 'People in the group chat quickly changed their topic, and they started making fun of another classmate in your class. Alan was still not in it, you find that Allen has become silent and often stays alone. You are wondering if you did something at that time, would the situation be different...')
+            this.result2 = this.add.text(500, game.config.height-200, 'People in the group chat quickly changed their topic, and they started making fun of another classmate in your class. Alan was still not in it, you find that Allen has become silent and often stays alone. You are wondering if you did something at that time, would the situation be different... Try it again to help Allan!',{
+                backgroundColor: "#EFCA8F",
+                padding: 15,
+                color: "black",
+                alpha: 1,
+                wordWrap: { width: 450, useAdvancedWrap: true }    
+            })
+            setTimeout(function(){ result2.alpha = 0}, 500)
         }else if(userResponse == this.answers[2]){
-            this.result3 = this.add.text(500, game.config.height-200, 'There was a period of silence in the group chat, but as time passed, this event passed. Allen became as happy as usual.')
+            this.result3 = this.add.text(500, game.config.height-250, 'There was a period of silence in the group chat, but as time passed, this event passed. Allen became as happy as usual.Congratulations!',{
+                backgroundColor: "#EFCA8F",
+                padding: 15,
+                color: "black",
+                alpha: 1,
+                wordWrap: { width: 450, useAdvancedWrap: true } 
+            })
+            setTimeout(function(){ result2.alpha = 0}, 500)
         }else{
-            this.result4 = this.add.text(500, game.config.height-200, 'Others also spoke in the group chat to express their support for you, Allen was invited into the group chat. Some people also said sorry to Allen.')
+            this.result4 = this.add.text(200, game.config.height-250, 'Others also spoke in the group chat to express their support for you, Allen was invited into the group chat. Some people also said sorry to Allen. Congratulations! ',{
+                backgroundColor: "#EFCA8F",
+                padding: 15,
+                color: "black",
+                alpha: 1,
+                wordWrap: { width: 700, useAdvancedWrap: true }  
+            })
+            setTimeout(function(){ result2.alpha = 0}, 500)
         }
     }
     
